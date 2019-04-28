f016e3b2c73aa3107a60aa01ae1d4e8e10ace775
Justin Duncan <falsemotive@icloud.com>
2019-04-28 15:17:18 -0600

    move config.ru to app according to heroku error

---
+++ test/features/user_sees_a_homepage_test.rb
@@ -0,0 +1,18 @@
+require './test/test_helper'
+
+class HomepageTest < CapybaraTestCase
+  def test_user_can_see_the_homepage
+    visit '/'
+
+    assert page.has_content?("Welcome!")
+    assert_equal 200, page.status_code
+  end
+
+  def test_user_can_see_error_page_when_visit_wrong_page
+    visit '/blogg'
+
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
+  end
+
+end
